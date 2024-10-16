626a33754a55041df4c9541517d58cc2f51cd5d9
SaaRanG <saarang3839@gmail.com>
2024-10-16 10:54:39 +0500

    Project

---
+++ script.js
@@ -0,0 +1,8 @@
+function openmenu() {
+    var el = document.getElementById("myNav");
+    if(el.style.height == "200px"){
+        el.style.height = "0px";
+    } else {
+        el.style.height = "200px";
+    }
+}
